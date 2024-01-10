38fe74a8d1736c668e043193d0e6d297138f6824
rorrxr <1314everu@gmail.com>
2024-01-10 13:32:28 +0900

    240110 오전

---
+++ java_basic/study/src/chapter2/IntegerVariable1.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class IntegerVariable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 정수형 int형 변수 iVal_1, iVal_2 선언한다.
+		// 정수값을 사용시 데이터타입은 int형을 기본으로 한다.
+		int iVal_1 = 10; // iVal_1 변수에 10을 저장한다.
+		int iVal_2 = 20; // iVal_2 변수에 20을 저장한다.
+		
+		// 정수형 int형 변수 result에 iVal_1 값과 iVal_2 값을 더한 값을 저장한다.
+		int result = iVal_1 + iVal_2;
+		
+		System.out.println(result); // result 값을 출력한다.
+	}
+
+}
