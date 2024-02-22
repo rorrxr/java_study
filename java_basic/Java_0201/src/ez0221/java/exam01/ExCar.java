4ed6e5d5068b49ce208755d9c6e52c053d1656aa
muchluvu <rorrxr@naver.com>
2024-02-22 09:48:50 +0900

    upload Java Study Day1

---
+++ java_basic/Java_0201/src/ez0221/java/exam01/ExCar.java
@@ -0,0 +1,12 @@
+package ez0221.java.exam01;
+
+public class ExCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new Car();
+		car.setA(1);
+		System.out.println("car.getA() : " + car.getA());
+	}
+
+}
